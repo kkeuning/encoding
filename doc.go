fcef4bc5fb05af55de51f4539361ea4ed8c6b890
Raphael Simon <simon.raphael@gmail.com>
2016-02-03 15:45:09 -0800

    Add doc stub

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package encoding provide goa adapters to many different encoders.
+
+Use the Consumes and Produces DSL in your design to specify the encoding supported by your
+services. See the corresponding documentation at
+https://godoc.org/github.com/goadesign/goa/design/apidsl#Consumes
+*/
+package encoding
